08922ada1d97cf82e98f2b88045af7c6b725bc81
Franck Fernandez <franck.fernandez@gmail.com>
2023-11-08 16:08:12 +0100

    Add links on coin page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+import { FaCode, FaGlobe } from 'react-icons/fa';
+import { BiSolidBook } from 'react-icons/bi';
 import { axios } from '@/axios';
 import { getCurrencyValue } from '@/helpers';
 import { GetCryptosMetadataRequest, GetCryptosRequest } from '@/type';
-import Image from 'next/image';
+import Link from 'next/link';
 
 const isPositive = (v: number) => v >= 0;
 const getColor = (v: number) =>
-  isPositive(v) ? 'text-red-400' : 'text-green-400';
+  isPositive(v) ? 'text-green-400' : 'text-red-400';
 
 export default async function Page({
   params: { id },
@@ -28,60 +31,89 @@ export default async function Page({
     name,
     quote: { USD },
   } = Object.values(market)[0];
-  const { logo, description } = Object.values(metadata)[0];
+  const { logo, description, urls } = Object.values(metadata)[0];
 
   return (
     <main className="space-y-14">
-      <div className="flex items-center gap-3 mb-14">
-        <Image width={34} height={34} src={logo} alt={`${name} icon`} />
-        <h1 className="text-4xl">
-          {name}
-          <span className="ml-5 text-2xl text-sub-color">
-            {getCurrencyValue(USD.price)}
-          </span>
-        </h1>
-      </div>
+      <section className="flex items-center justify-between mb-14">
+        <div className="flex items-center gap-3">
+          <Image width={34} height={34} src={logo} alt={`${name} icon`} />
+          <h1 className="text-4xl">
+            {name}
+            <span className="ml-5 text-2xl text-sub-color">
+              {USD.price < 0.1 ? `$${USD.price}` : getCurrencyValue(USD.price)}
+            </span>
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-3">
+          {urls.website.map((url, idx) => (
+            <Link href={url} key={idx} target="_blank">
+              <FaGlobe />
+            </Link>
+          ))}
+          {urls.source_code.map((url, idx) => (
+            <Link href={url} key={idx} target="_blank">
+              <FaCode />
+            </Link>
+          ))}
+          {urls.technical_doc.map((url, idx) => (
+            <Link href={url} key={idx} target="_blank">
+              <BiSolidBook />
+            </Link>
+          ))}
+        </div>
+      </section>
 
       <p>{description}</p>
 
-      <div className="grid grid-cols-3 [&_div]:mx-auto [&_div]:p-10 [&_div]:flex [&_div]:flex-col [&_div]:items-center">
+      <section className="grid grid-cols-3 [&_div]:mx-auto [&_div]:p-10 [&_div]:flex [&_div]:flex-col [&_div]:items-center text-lg">
         <div>
           <span className={getColor(USD.percent_change_1h)}>
             {USD.percent_change_1h.toFixed(2)}%
           </span>
-          <span>1h%</span>
+          <span>1h</span>
         </div>
         <div>
           <span className={getColor(USD.percent_change_24h)}>
             {USD.percent_change_24h.toFixed(2)}%
           </span>
-          <span>24h%</span>
+          <span>24h</span>
         </div>
         <div>
           <span className={getColor(USD.percent_change_7d)}>
             {USD.percent_change_7d.toFixed(2)}%
           </span>
-          <span>7d%</span>
+          <span>7d</span>
         </div>
         <div>
           <span className={getColor(USD.percent_change_30d)}>
             {USD.percent_change_30d.toFixed(2)}%
           </span>
-          <span>30d%</span>
+          <span>30d</span>
         </div>
         <div>
           <span className={getColor(USD.percent_change_60d)}>
             {USD.percent_change_60d.toFixed(2)}%
           </span>
-          <span>60d%</span>
+          <span>60d</span>
         </div>
         <div>
           <span className={getColor(USD.percent_change_90d)}>
             {USD.percent_change_90d.toFixed(2)}%
           </span>
-          <span>90d%</span>
+          <span>90d</span>
         </div>
-      </div>
+
+        <div>
+          <span>{USD.market_cap_dominance.toFixed(2)}%</span>
+          <span>Dominance</span>
+        </div>
+        <div>
+          <span>{getCurrencyValue(USD.market_cap)}</span>
+          <span>Market Cap</span>
+        </div>
+      </section>
     </main>
   );
 }
--- helpers.ts
+++ helpers.ts
@@ -1,1 +1,2 @@
-export const getCurrencyValue = (v: number) => `$${new Intl.NumberFormat('en-US').format(v)}`;
+export const getCurrencyValue = (v: number) =>
+  v < 0.1 ? `$${v}` : `$${new Intl.NumberFormat('en-US').format(v)}`;
