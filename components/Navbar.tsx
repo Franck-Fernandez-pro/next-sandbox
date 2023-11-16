12887d804dac59ddb5d8cadf2e581e8bd5639d85
Franck Fernandez <franck.fernandez@gmail.com>
2023-11-16 15:58:15 +0100

    Update <Navbar />

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
-import { ConnectButton } from './ConnectButton';
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <nav className="fixed -mt-20 -mx-9 p-5 flex justify-end w-full gap-3">
-      {/* <Link href="/me">
-        <FaUser />
-      </Link> */}
-      <ConnectButton />
+    <nav className="fixed -mt-20 -mx-9 p-5 flex justify-center w-full gap-5">
+      <Link className={`${pathname === '/' ? '' : 'text-text-color'}`} href="/">
+        Currencies
+      </Link>
+      <Link
+        className={`${pathname === '/explore' ? '' : 'text-text-color'}`}
+        href="/explore"
+      >
+        Explore
+      </Link>
     </nav>
   );
 }
